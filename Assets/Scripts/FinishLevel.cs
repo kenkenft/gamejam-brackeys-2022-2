9d2024afb81492e049a318682597501e651ecd17
Ken Tang <kenftang@gmail.com>
2022-08-25 22:32:12 +0100

    Add FinishLevel class Modify PlayerChange class
    FinishLevel class triggers the next scene to load. Currently, it
    prints a debug message.
    
    PlayerChange has been modified to catch errors when setting up totems.
    It also sets up the totemIDs automatically.

---
+++ Assets/Scripts/FinishLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLevel : MonoBehaviour
+{
+    public string nextSceneName;
+    public bool lastLevel;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("FinishLevel");
+        if(other.CompareTag("Totem"))
+        {
+            
+            //Load relevant scene
+            if(lastLevel == true)
+            {
+                Debug.Log("Last Level completed: You Win!");
+                // SceneManager.LoadScene(6); //Loads EndScreen
+            }
+            else
+            {
+                Debug.Log("Level completed: Load next level");
+                // SceneManager.LoadScene(nextSceneName);
+            }
+            
+
+        }
+    }
+}
--- Assets/Scripts/PlayerChange.cs
+++ Assets/Scripts/PlayerChange.cs
@@ -15,21 +15,57 @@ public class PlayerChange : MonoBehaviour
     
     void Awake()
     {
+        try
+        {
+            Debug.Log(listTotems[0].name);
+        }
+        catch
+        {
+            Debug.Log("totems have not been set in PlayerChange.listTotems");
+        }
+
         maxTotemCount = listTotems.Count;
+        // GameObject[] allTotemsArray = GameObject.FindGameObjectsWithTag("Totem");
         SetUpTotems();
     }
-        
+    
+    void SetUpTotems()
+    {
+        for(counter = 0; counter < maxTotemCount; counter++)
+        {
+            listClassPlayerMove.Add(listTotems[counter].GetComponentInChildren<PlayerMove>());
+            listClassStackTotem.Add(listTotems[counter].GetComponentInChildren<StackTotem>());
+            listRigs.Add(listClassPlayerMove[counter].gameObject.GetComponent<Rigidbody2D>());
+            // listClassPlayerMove.Add(allTotemsArray[counter].GetComponentInChildren<PlayerMove>());
+            // listClassStackTotem.Add(allTotemsArray[counter].GetComponentInChildren<StackTotem>());
+            // listRigs.Add(listClassPlayerMove[counter].gameObject.GetComponent<Rigidbody2D>());
+
+            listClassStackTotem[counter].totemID = counter;
+
+            listTotemIDs.Add(listClassStackTotem[counter].totemID);
+            if(listTotemIDs[counter] != 0)
+            {
+                listClassStackTotem[counter].isTotemRecruited = false;
+                SetTotemState(false);
+            }
+            else
+            {
+                listClassStackTotem[counter].isTotemRecruited = true;
+                SetTotemState(true);
+            }
+            listClassStackTotem[counter].isTotemStacked = false;
+            // PrintTotemState();
+        }
+        counter = 0;
+    }
+
     public void ChangeCharacter()
     {
         Debug.Log("ChangeCharacter called!");
         if(maxTotemCount > 1)
         {
-            // listRigs[counter].isKinematic = true;
-            // listClassPlayerMove[counter].totemState = "inactive";
-            // listClassPlayerMove[counter].enabled = false; // Should disable current active totem
             SetTotemState(false);
             SwitchNextAvailableTotem();
-            // Debug.Log("Controlling Totem " + listClassStackTotem[counter].totemID);
         }
     }
 
@@ -38,37 +74,7 @@ public class PlayerChange : MonoBehaviour
         return maxTotemCount;
     }
 
-    void SetUpTotems()
-    {
-        for(counter = 0; counter < maxTotemCount; counter++)
-        {
-            listClassPlayerMove.Add(listTotems[counter].GetComponentInChildren<PlayerMove>());
-            listClassStackTotem.Add(listTotems[counter].GetComponentInChildren<StackTotem>());
-            listRigs.Add(listClassPlayerMove[counter].gameObject.GetComponent<Rigidbody2D>());
-            // listCols.Add(listClassPlayerMove[i].gameObject.GetComponent<Collider2D>());
-
-            listTotemIDs.Add(listClassStackTotem[counter].totemID);
-            if(listTotemIDs[counter] != 0)
-            {
-                // Debug.Log(listClassPlayerMove[counter].name + ": Totem ID not 0");                // listRigs[i].isKinematic = true;
-                listClassStackTotem[counter].isTotemRecruited = false;
-                // listRigs[counter].isKinematic = true;
-
-                SetTotemState(false);
-            }
-            else
-            {
-                // Debug.Log(listClassPlayerMove[counter].name + ": Totem ID is 0");                // listRigs[i].isKinematic = true;
-                listClassStackTotem[counter].isTotemRecruited = true;
-                // listRigs[counter].isKinematic = false;
-
-                SetTotemState(true);
-            }
-            listClassStackTotem[counter].isTotemStacked = false;
-            // PrintTotemState();
-        }
-        counter = 0;
-    }
+    
 
     private void SwitchNextAvailableTotem()
     {
@@ -82,20 +88,15 @@ public class PlayerChange : MonoBehaviour
 
             if(listClassStackTotem[counter].isTotemRecruited && !listClassStackTotem[counter].isTotemStacked)
             {
-                // listRigs[counter].isKinematic = false;
-
                 SetTotemState(true);
                 nextTotemFound = true;
             }
         }
-        // return currCount;
     }
 
     private void SetTotemState(bool state)
     {
         listClassStackTotem[counter].totemState = state ? "active" : "inactive";
-        // listRigs[counter].isKinematic = !state;
-        // listCols[counter].isTrigger = !state;
         listClassPlayerMove[counter].enabled = state;
     }
 
@@ -111,13 +112,11 @@ public class PlayerChange : MonoBehaviour
         // Debug.Log("ChangeToTargetTotem called!");
 
         counter = listTotemIDs.IndexOf(currTotem.totemID);
-        // Debug.Log("currTotem returned index: " + counter);
         SetTotemState(false);
 
         counter = listTotemIDs.IndexOf(nextTotem.totemID);
         listRigs[counter].isKinematic = false;
         listClassStackTotem[counter].isTotemStacked = false;
-        // Debug.Log("currTotem returned index: " + counter);
         SetTotemState(true);
     }
 }
