02f2f187627676bfa665f7c53ea13273e58ac0ce
Ken Tang <kenftang@gmail.com>
2022-08-27 18:19:46 +0100

    Add SetUpBoundaries to CameraSetUp
    SetUpBoundaries sets up boxcolliders around the edges of the level to
    prevent the player from leaving the boundaries. This is dependent on
    lowerLeftBound and upperRightBound. The resulting configuration of
    colliders should box the player into the level.

--- Assets/Scripts/CameraSetUp.cs
+++ Assets/Scripts/CameraSetUp.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class CameraSetUp : MonoBehaviour
 {
-    public GameObject lowerLeftBound, upperRightBound;
+    public GameObject lowerLeftBound, upperRightBound; 
+    public GameObject[] levelBoundary; // 0 - Left; 1 - Right; 2 - Up; 3 - Bottom
     private Vector3 cameraPos = new Vector3(0f,0f,0f), mask = new Vector3(0f,0f,0f); 
-    // private Camera mainCamera;
-    // Start is called before the first frame update
+    private BoxCollider2D targetCol;
+
     void Start()
     {
-        // mainCamera = GetComponentInChildren<Camera>();
+        mask[0] = upperRightBound.transform.position.x - lowerLeftBound.transform.position.x;
+        mask[1] = upperRightBound.transform.position.y - lowerLeftBound.transform.position.y;
         SetUpCamera();
+        SetUpBoundaries();
     }
 
     void SetUpCamera()
     {
-        mask[0] = upperRightBound.transform.position.x - lowerLeftBound.transform.position.x;
-        mask[1] = upperRightBound.transform.position.y - lowerLeftBound.transform.position.y;
         cameraPos = Vector3.Lerp(lowerLeftBound.transform.position, upperRightBound.transform.position, 0.5f);
         // cameraPos[0] = mask[0]/2f;
         // cameraPos[1] = mask[1]/2f;
@@ -34,5 +35,53 @@ public class CameraSetUp : MonoBehaviour
             float differenceInSize = targetRatio/screenRatio;
             Camera.main.orthographicSize = mask[1] / 2f * differenceInSize;
         }
+    }
+    void SetUpBoundaries()
+    {
+        Vector3 scaleMaskX = new Vector3(mask[0], 1f, 0f);
+        Vector3 scaleMaskY = new Vector3(1f, mask[1], 0f); 
+        Vector3 posLeftMask = new Vector3(lowerLeftBound.transform.position.x, lowerLeftBound.transform.position.y + (mask[1]/2f), 0f);
+        Vector3 posRightMask = new Vector3(upperRightBound.transform.position.x, lowerLeftBound.transform.position.y + (mask[1]/2f), 0f); 
+        Vector3 posTopMask = new Vector3(0f, lowerLeftBound.transform.position.y + mask[1], 0f);
+        Vector3 posBottomMask = new Vector3(0f, lowerLeftBound.transform.position.y, 0f); 
+
+        for(int i = 0; i < levelBoundary.Length; i++)
+        {
+            targetCol = levelBoundary[i].GetComponent<BoxCollider2D>();
+            switch(i)
+            {
+                case 0:
+                {
+                    targetCol.size = scaleMaskY;
+                    targetCol.transform.position = posLeftMask;
+                    break;
+                }
+                case 1:
+                {
+                    targetCol.size = scaleMaskY;
+                    targetCol.transform.position = posRightMask;
+                    break;
+                }
+                case 2:
+                {
+                    targetCol.size = scaleMaskX;
+                    targetCol.transform.position = posTopMask;
+                    break;
+                }
+                case 3:
+                {
+                    targetCol.size = scaleMaskX;
+                    targetCol.transform.position = posBottomMask;
+                    break;
+                }
+                default:
+                {
+                    Debug.Log("Error in setting boundary");
+                    break;
+                }            
+            }
+        }
+
+
     }
 }
